79281e47f891b0ef805ef952f8d9f75df97c06d1
Frank <fsolleveld@outlook.com>
2022-03-07 22:36:16 +0100

    Initial Commit

---
+++ GeoMap/GeoMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoMapApp.swift
+//  GeoMap
+//
+//  Created by Frank Solleveld on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeoMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
