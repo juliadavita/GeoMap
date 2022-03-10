0e53ec5598bed50299434eebcf4f177e82404322
Julia Lanu <julialanu@hotmail.com>
2022-03-10 15:57:17 +0100

    First commit

---
+++ GeoMap/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  GeoMap
+//
+//  Created by Frank Solleveld on 08/03/2022.
+//
+
+import Foundation
---
+++ GeoMap/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  GeoMap
+//
+//  Created by Frank Solleveld on 08/03/2022.
+//
+
+import Foundation
